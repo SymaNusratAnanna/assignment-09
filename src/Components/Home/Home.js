676234c5b6573afc931ffebf55b0104ace40e45f
SymaNusratAnanna <symanusrat158@gmail.com>
2022-04-05 22:39:41 +0600

    homepage to review page route complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useReview from '../../hook/useReview';
 import Review from '../Review/Review';
 
 const Home = () => {
     const [reviews] = useReview();
+    const navigate = useNavigate();
     return (
         // <div className='flex justify-between items-center mt-5 mx-24 align-middle'>
         <div className='grid grid-cols-2 gap-4 ml-24 mt-12'>
@@ -24,7 +26,7 @@ Wear Fantasy is a brand where you can buy your desirabele fashion wear!!Grave th
 reviews.slice(0,3).map(review=><Review key = {review.id} review= {review}></Review>)
         }
     </div>
-    <button className='p-3 bg-blue-600 rounded-xl text-3xl text-white'> Show all reviews</button>
+    <button onClick={()=>navigate('/reviews')} className='p-3 bg-blue-600 rounded-xl text-3xl text-white'> Show all reviews</button>
 </div>
 
         </div>
